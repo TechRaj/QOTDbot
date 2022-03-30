3b92149e9f57499fdd1386be93658b1bab37e4f9
Karthikraj Sivakumar <techraj0207@gmail.com>
2022-03-30 19:25:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 import discord
 import os
-from discord.ext import commands, tasks
-import asyncio
-from discord.ext import commands, tasks
-from datetime import datetime, timedelta
+import json
+import requests
+import aiocron
 
 my_secret = os.environ['TOKEN']
+channel_id = 1234
 
 client = discord.Client()
+
+def get_question():
+    response = requests.get('https://api.api-ninjas.com/v1/trivia?category=general', headers={'X-Api-Key': 'MY_API_KEY'})
+    json_data = json.loads(response.text)
+    question = json_data[0]['question'] + "?"
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print("Error:", response.status_code, response.text)
+    return(question)
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-def seconds_until_ten():
-    now = datetime.now()
-    target = (now + timedelta(days=1)).replace(hour=14, minute=00, second=0, microsecond=0)
-    diff = (target - now).total_seconds()
-    print(f"{target} - {now} = {diff}")
-    return diff
+@aiocron.crontab('14 11 * * *')
+async def cronjob1():
+    channel = client.get_channel(channel_id)
+    question = get_question()
+    await channel.send(question)
+    return
 
-@tasks.loop(seconds=1)
-async def called_once_a_day_at_ten():
-    await asyncio.sleep(seconds_until_ten())
-    message_channel = client.get_channel(my_secret)
-    print(f"Got channel {message_channel}")
-    await message_channel.send("QOTD: blah blah blah")
-
-@called_once_a_day_at_ten.before_loop
-async def before():
-    await client.wait_until_ready()
-    print("Finished waiting")
-
-called_once_a_day_at_ten.start()
 client.run(my_secret)
